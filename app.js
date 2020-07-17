20859285c96717d0a7d7d5e185e56c8022cb83e2
Elaine <elaine@Felipes-iMac.lan1>
2020-07-17 16:11:41 -0500

    [app] adds my data types.

---
+++ app.js
@@ -0,0 +1,17 @@
+var nuts = ['peanut', 'cashew', 'almond'];
+console.log(nuts);
+var statement = 'I like to play boardgames';
+console.log(statement);
+var relationship = {
+    partner: 'felipe',
+    time: 5,
+    vacations: ['Paris', 'Spain'],
+    areTheBest: true
+};
+console.log(relationship);
+
+let doesThisWork = true;
+console.log(doesThisWork);
+
+var numberOfDogs = 2;
+console.log(numberOfDogs);
