e28f6840fb88822662a6f7686691db82e281f623
Elaine <elaine@Felipes-iMac.lan1>
2020-07-17 16:37:04 -0500

    [functions] adds functions to lesson

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var nuts = ['peanut', 'cashew', 'almond'];
 console.log(nuts);
+
 var statement = 'I like to play boardgames';
 console.log(statement);
+
 var relationship = {
     partner: 'felipe',
     time: 5,
@@ -15,3 +17,18 @@ console.log(doesThisWork);
 
 var numberOfDogs = 2;
 console.log(numberOfDogs);
+
+function food(object){
+    console.log(object[1]);
+}
+
+function dtr(relationship){
+    relationship.pop();
+}
+dtr(relationship(status:yes));
+function moreDogs(num){
+    return num + 1
+}
+console.log(moreDogs);
+
+function add
