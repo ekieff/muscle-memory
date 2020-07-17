215400cd5b4db882e10fa2a23e604489b0420f7c
Elaine <elaine@Felipes-iMac.lan1>
2020-07-17 18:16:53 -0500

    [containerSeven] adds working alone code.

--- app.js
+++ app.js
@@ -70,10 +70,57 @@ const paraThree = document.querySelector('.paragraph-three');
 console.log(paraThree);
 paraThree.textContent = 'Lerner';
 
-const paraFour = document.querySelector('.paragrah-four');
+const paraFour = document.querySelector('.paragraph-four');
 console.log(paraFour);
 paraFour.textContent = 'Kieffer';
 
 const paraFive = document.querySelector('.paragraph-five');
 console.log(paraFive);
 paraFive.textContent = 'Samsung';
+
+const containerSix = document.createElement('div');
+
+containerSix.classList.add('container-six');
+console.log(containerSix);
+const sectionSix = document.createElement('h2');
+sectionSix.setAttribute('id', 'section-six');
+sectionSix.textContent = 'Hello';
+console.log(sectionSix);
+
+const paraSix = document.createElement('p');
+paraSix.classList.add('paragraph-six');
+paraSix.textContent = 'James Harden';
+console.log(paraSix);
+
+const body = document.querySelector('body');
+console.log(body);
+
+body.appendChild(containerSix);
+containerSix.appendChild(sectionSix);
+containerSix.appendChild(paraSix);
+
+// create another container
+const containerSeven = document.createElement('div');
+console.log(containerSeven);   
+// add class
+containerSeven.classList.add('container-seven');
+console.log(containerSeven);
+// create another section
+const sectionSeven = document.createElement('h2');
+   // add an id
+sectionSeven.setAttribute('id', 'section-seven');
+   // add textContent
+sectionSeven.textContent = 'Goodbye';
+// create another paragraph
+const paraSeven = document.createElement('p');
+    // add a class
+paraSix.classList.add('paragraph-seven');
+    // add textContent
+paraSeven.textContent = 'Pinnochio';
+// append the container to the body
+body.appendChild(containerSeven);
+// append section to the container
+containerSeven.appendChild(sectionSeven);
+// append the paragraph to the container
+containerSeven.appendChild(paraSeven);
+console.log(containerSeven);
